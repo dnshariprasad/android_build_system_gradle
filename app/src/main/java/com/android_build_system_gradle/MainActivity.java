d6cc638f7be4b92ccab58b5c2cb6fbcfcaac1612
Hari Prasad <dnshariprasad@gmail.com>
2016-12-20 01:53:12 +0530

    first commit

---
+++ app/src/main/java/com/android_build_system_gradle/MainActivity.java
@@ -0,0 +1,15 @@
+package com.android_build_system_gradle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.tv_product_flavors)).setText(Constant.THIS_BUILD);
+    }
+}
---
+++ app/src/paid/java/com/android_build_system_gradle/Constant.java
@@ -0,0 +1,9 @@
+package com.android_build_system_gradle;
+
+/**
+ * Created by Hari Prasad on 12/20/16.
+ */
+
+public class Constant {
+    public static final String THIS_BUILD = "PAID";
+}
